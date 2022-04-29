d3ab88f13142b6ab2e5cc7d4a6f2a743f0362083
mays4 <maysalani84@yahoo.com>
2022-04-29 03:36:02 -0400

    adding colors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
         if (typeof quotes === "string") {
           quotesData = JSON.parse(quotes);
         }
-        renderQuotes(quotesData.quotes);
+        
       },
       error: (err) => {
         console.log(`there was an error: ${err}`);
@@ -25,23 +25,37 @@ $(document).ready(() => {
   const renderQuotes = (quotes) => {
     let rendomIndex = Math.floor(Math.random() * quotes.length);
     let randomQuoteData = quotes[rendomIndex];
-
-    author = randomQuoteData.author;
+    let randomcolor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+                        $('body').css('background-color', randomcolor);
+    
     quoteTitle = randomQuoteData.quote;
+    author = randomQuoteData.author;
+    // let textQuote = document.getElementById("text").innerText = quoteTitle;
+    //  let authorQuote= document.getElementById("author").innerText = author;
+      
+     $("#text").html(quoteTitle).css("color", randomcolor);
+     $("#author").html(randomQuoteData.author).css("color", randomcolor);
+     $("#new-quote").css("background-color", randomcolor);
+     $("#tweet-quote").css("color", randomcolor);
+     $("#tumbler-quote").css("color", randomcolor);
+     $("#quote-text").css("color", randomcolor);
+     
+
   };
 
 
-  const generateRandomQuote = () => {
-    $("#new-quote").on("click", function () {
+
+  // const generateRandomQuote = () => {
+    $('#new-quote').click(function () {
+
+      renderQuotes(quotesData.quotes);
+  
       let tweetQuate = quoteTitle.replace(/ /g, "%20");
       let tweetAuthor = author.replace(/ /g, "%20");
       let tweetLink = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text= ${tweetQuate} + - + ${tweetAuthor} +  `;
       console.log(tweetLink);
+     
       document.getElementById("tweet-quote").href = tweetLink;
-      document.getElementById("text").innerText = quoteTitle;
-      document.getElementById("author").innerText = author;
     });
-  };
   
-  generateRandomQuote();
 });
